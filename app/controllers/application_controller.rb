009413ee247f563e3b0879f80d33fd1b07b7e092
wilfried ndomo kenfack <wilfried.ndomo@gmail.com>
2021-06-25 03:06:40 +0200

    end registration instructions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  def user_credentials
+    keys = %w[nome cognome email password]
+    user_params = select_params(params, keys)
+    user_params
+  end
+
+  # This method takes the params receive from Axiom and then select those necessary for creating an object.
+  def select_params params, keys
+    model ={}
+    params.each do |key, value|
+      if keys.include? key
+        model[key]=value
+      end
+    end
+    model
+  end
 end
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+
+  # POST /resource
+  # def create
+  #   super
+  # end
+  def create
+    if User.find_by_email(user_credentials["email"]).present?
+      render json: { message: "Indirizzo e-mail già utilizzato", flag: 3} # mail already assigned to a user
+    else
+      user = User.new(user_credentials)
+      if user.save
+        render json: { message: "Riceverà una mail con le istruzioni su come confermare il suo indirizzo email in pochi minuti", flag: 1}
+      else
+        render json: { message: "Si è verificato un errore durante la registrazione. Verifichi i dati inseriti" , flag: 2}
+      end 
+    end
+  end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+end
